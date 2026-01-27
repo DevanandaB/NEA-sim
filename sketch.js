908672b209e94eed926862623cd5ee5102e80848
Devananda <deva31807@gmail.com>
2026-01-27 18:22:17 +0000

    Designing neutron source

--- sketch.js
+++ sketch.js
@@ -24,9 +24,6 @@ async function setup() {
     colorMode(HSB, 360, 100, 100);
     palette = 1;
     getColor(3);
-
-    // stroke('white');
-    // strokeWeight(4);
 }
 
 //to get color from color palette
@@ -40,7 +37,6 @@ function draw() {
     background(h, s, b);
     orbitControl();
     fill(255);
-
     push();
     beginShape(LINES);
     vertex(220, -20);
@@ -50,7 +46,10 @@ function draw() {
     endShape();
     noFill();
     bezier(220, -20, 240, -28, 240, -142, 220, -150);
-    bezier(-180, 10, -120, 2, -120, -168, -180, -160);
+    ellipse(-185, -75, 100, 170); //use ellipsoid later 
+    ellipse(-195, -75, 80, 160); 
+    ellipse(-200, -75, 40, 40);
+    ellipse(-200, -75, 15, 15); 
     pop();
 
     push();
@@ -64,12 +63,13 @@ function draw() {
     bezier(300, -55, 320, -50, 320, -115, 300, -120);
     pop();
 
-    // push();
-    // strokeWeight(5);
-    // stroke('black');
-    // ellipse(310, -80, 50, 180);
-    // ellipse(340, -80, 50, 160);
-    // ellipse(370, -80, 40, 140);
-    // pop();
-
+    push();
+    noFill();
+    bezier(-70, 200, -120, 150, -30, 50, 100, -10);
+    bezier(-195, 200, -165, 140, -125, 90, -175, 10);
+    beginShape(LINES);
+    vertex(-195, 200);
+    vertex(-70, 200);
+    endShape();
+    pop();
 }
