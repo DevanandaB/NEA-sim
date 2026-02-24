96324416e1d54374d0bb052fb7fb890f8de9022f
Devananda <deva31807@gmail.com>
2026-02-24 19:44:17 +0000

    Designing particles

--- particle.js
+++ particle.js
@@ -1,68 +1,75 @@
-let neutron;
+let neutron1;
 let vesselRadius;
-let detail;
-let r = 200;
 
 function setup() {
     const canvas = createCanvas(750, 600, WEBGL);
     canvas.parent('sim-container');
     angleMode(DEGREES);
-    let position = createVector(-130, 0, 0);
-    neutron = new Particle(position, red);
+    let position1 = createVector(-120, 0, 0);
+    neutron1 = new Neutron(position1, 5, 'red');
 }
 
 function draw() {
     background('#9CAFB7');
     orbitControl();
-    // (fill(255, 200, 100, 0.5))
-    // translate(100, 0, 0);
-    // sphere(220);
-    stroke('red');
-    strokeWeight(4);
-    beginShape(POINTS);
-    for (let j = 0; j < 30; j++) {
-      for (let i = 0; i < 180; i += 2) {
-        let x = r * cos(i);
-        let y = r * sin(i);
-        let z = 0;
-        vertex(x, y, z);
-    }
-    }
-    endShape();
 
-    // neutron.checkEdges();
-    // neutron.display();
-    // neutron.update();
+    //particles move within the boundary 
+    push();
+    noFill();
+    translate(100, 0, 0);
+    sphere(220);
+    pop();
+    
+    push();
+    neutron1.display();
+    neutron1.fire();
+    neutron1.checkEdges();
+    pop();
+    
 }
 
 class Particle {
-  constructor(pos, color) {
+  constructor(pos, radius, color) {
     this.pos = pos;
-    this.vel = new createVector(random(0, 0.5), 0);
-    this.acc = new createVector(random(0,1), 0);
-    this.radius = 10;
+    // this.vel = new createVector(random(0, 0.5), 0);
+    // this.acc = new createVector(random(0,1), 0);
+    this.radius = radius;
     this.color = color;
-    }
-  
-  display() {
-    fill(0);
-    ellipse(this.pos.x, this.pos.y, this.radius);
   }
-  
-  update() {
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
+
+  display() {
+    //fill color
+    fill(this.color);
+    translate(this.pos.x, this.pos.y);
+    sphere(this.radius);
   }
    
   checkEdges() {
-    if(this.pos.x > 335) {
+    if(this.pos.x > 325) {
       this.vel = new createVector(random(-0, -0.5), 0);
       this.acc =  new createVector(random(-0, -0.5), 0);
     }
-    if(this.pos.x < -135) {
+    if(this.pos.x < -120) {
       this.vel = new createVector(random(0, 0.5), 0);
       this.acc =  new createVector(random(0, 0.5), 0);
     }
   }
-  
+
 }
+
+class Neutron extends Particle {
+  constructor(pos, radius, color) {
+    super(pos, radius, color);
+    this.vel = new createVector(random(0, 0.5), 0);
+    this.acc = new createVector(random(0,1), 0);
+  }
+
+  fire() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+  }
+
+}
+
+Neutron.prototype = Object.create(Particle.prototype);
+
